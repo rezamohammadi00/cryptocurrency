f4e68b127124523bd15e993de4069f36b8e15a8d
Rez <mohammadireza7979.com>
2024-12-08 16:09:54 +0330

    v.2.5.2

---
+++ src/hooks/useGetFontSize.ts
@@ -0,0 +1,15 @@
+import { Grid } from "antd";
+
+const { useBreakpoint } = Grid;
+
+function useGetFontSize() {
+  const screens = useBreakpoint();
+
+  if (screens.xs) return "25px"; // Extra small screens
+  if (screens.sm) return "30px"; // Small screens
+  if (screens.md) return "30px"; // Medium screens
+  if (screens.lg) return "30px"; // Large screens
+  return "28px"; // Extra large screens
+}
+
+export default useGetFontSize;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import useGetCoins from "../hooks/useGetCoins.ts";
 import Card from "../components/Card";
 import { Link } from "react-router-dom";
 import Loader from "../components/Loader.tsx";
+import useGetFontSize  from "../hooks/useGetFontSize.ts";
 
 const formatValue = (value: number, title: string = "") => {
   const isCurrency =
@@ -28,6 +29,7 @@ const HomePage = () => {
   const { data, isLoading } = useGetCoins();
   const stats = data?.data?.stats;
   const coins: coin[] = data?.data?.coins;
+  const fontSize = useGetFontSize();
   // console.log(data)
   // console.log(coins)
 
@@ -35,7 +37,8 @@ const HomePage = () => {
   return (
     <Layout.Content className="overflow-y-scroll p-4 w-full h-full">
       {/*Global Crypto Stats section*/}
-      <Typography.Title level={2}>Global Crypto Stats</Typography.Title>
+      {/* <Typography.Title level={2}>Global Crypto Stats</Typography.Title> */}
+      <Typography.Title  style={{ fontSize: fontSize }}>Global Crypto Stats</Typography.Title>
       <Row gutter={[16, 20]} className="mt-3">
         {stats &&
           Object.entries(stats)?.map(([key, value]) => (
@@ -52,7 +55,10 @@ const HomePage = () => {
 
       {/*Top 10 section*/}
       <div className={"flex justify-between items-center mt-10"}>
-        <Typography.Title level={2}>
+        {/* <Typography.Title level={2}>
+          Top 10 Cryptos In The World
+        </Typography.Title> */}
+        <Typography.Title style={{ fontSize: fontSize }}>
           Top 10 Cryptos In The World
         </Typography.Title>
         <Typography.Link>
