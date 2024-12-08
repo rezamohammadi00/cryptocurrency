f8f9353acebd37fadb8cad297f7c70f560e097d9
Rez <mohammadireza7979.com>
2024-12-08 16:38:04 +0330

    v2.6

--- src/pages/CryptocurrenciesPage.tsx
+++ src/pages/CryptocurrenciesPage.tsx
@@ -1,70 +1,71 @@
-import {useEffect, useState} from "react";
-import {Col, Layout, Row, Typography, Input} from "antd";
-import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Col, Layout, Row, Typography, Input } from "antd";
+import { Link } from "react-router-dom";
 import millify from "millify";
 
 import useGetCoins from "../hooks/useGetCoins.ts";
 import Card from "../components/Card.tsx";
+import Loader from "../components/Loader.tsx";
 
-
-type  coin = {
-    uuid: string,
-    name: string,
-    iconUrl: string,
-    price: string,
-    marketCap: string,
-    change: string
-}
-
-
-const CryptocurrenciesPage = () => {
-    const {data,isLoading} = useGetCoins();
-    const coins: coin[] = data?.data?.coins;
-
-    const [filteredCryptocurrencies, setFilteredCryptocurrencies] = useState(coins)
-    const [searchingText, setSearchingText] = useState("")
-
-    useEffect(() => {
-        setFilteredCryptocurrencies(coins.filter((coin)=>{
-            return coin.name.toLowerCase().includes(searchingText.toLowerCase())
-        }))
-    }, [coins,searchingText]);
-
-
-    if (isLoading) {
-        return (
-          <div className="flex justify-center items-center w-full h-full">
-            Loading...
-          </div>
-        );
-      }
-
-    return (
-        <Layout.Content className="overflow-y-scroll p-4 w-full h-full">
-            <div className={"flex justify-between items-center"}>
-                <Typography.Title level={2}>Cryptos</Typography.Title>
-                <Input.Search size={"middle"} placeholder="Search Cryptocurrency" className={"w-52"} onSearch={setSearchingText}/>
-            </div>
-            <Row gutter={[32, 32]} className={"mt-3"}>
-                {filteredCryptocurrencies?.map((coin) => (
-                        // Coin Card
-                        <Col key={coin.uuid} span={6} xs={24} sm={12} lg={6}>
-                            <Link to={"/news"}>
-                                <Card
-                                    title={coin.name}
-                                    avatarSrc={coin.iconUrl}
-                                    price={millify(Number(coin.price))}
-                                    marketCap={millify(Number(coin.marketCap))}
-                                    change={coin.change}
-                                />
-                            </Link>
-                        </Col>
-                    )
-                )}
-            </Row>
-        </Layout.Content>
-    );
+type Coin = {
+  uuid: string;
+  name: string;
+  iconUrl: string;
+  price: string;
+  marketCap: string;
+  change: string;
 };
 
+const CryptocurrenciesPage = () => {
+  const { data, isLoading } = useGetCoins();
+  const coins: Coin[] = data?.data?.coins || [];
+
+  const [filteredCryptocurrencies, setFilteredCryptocurrencies] =
+    useState<Coin[]>(coins);
+  const [searchingText, setSearchingText] = useState<string>("");
+
+  // Filter coins based on the search query
+  useEffect(() => {
+    const filtered = coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchingText.toLowerCase())
+    );
+    setFilteredCryptocurrencies(filtered);
+  }, [coins, searchingText]);
+
+  // Render a loading state
+  if (isLoading) return <Loader />;
+
+  return (
+    <Layout.Content className="overflow-y-scroll p-4 w-full h-full">
+      {/* Header Section */}
+      <div className="flex justify-between items-center">
+        <Typography.Title level={2} className="">Cryptos</Typography.Title>
+        <Input.Search
+          size="middle"
+          placeholder="Search Cryptocurrency"
+          className="w-52"
+          onSearch={(value) => setSearchingText(value)}
+        />
+      </div>
+
+      {/* Crypto Cards Section */}
+      <Row gutter={[32, 32]} className="mt-3">
+        {filteredCryptocurrencies.map((coin) => (
+          <Col key={coin.uuid} span={6} xs={24} sm={12} lg={6}>
+            <Link to={`/news`}>
+              <Card
+                title={coin.name}
+                avatarSrc={coin.iconUrl}
+                price={millify(Number(coin.price))}
+                marketCap={millify(Number(coin.marketCap))}
+                change={coin.change}
+              />
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Layout.Content>
+  );
+};
 
 export default CryptocurrenciesPage;
